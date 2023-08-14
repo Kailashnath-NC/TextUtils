0dcca8c455d561b3311a99c836b0d6d0dcda6035
KailashnathNC <114307951+KailashnathNC@users.noreply.github.com>
2023-08-14 18:02:51 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               {/* <Link className="nav-link active" aria-current="page" to="/"> */}
-              <a className="nav-link active" aria-current="page" href="#">
+              <a className="nav-link" aria-current="page" href="#">
                 Home
               </a>
             </li>
